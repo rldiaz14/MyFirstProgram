7acb78d7f3fd8171d1e1ee83bf4aa98566a9bddd
rldiaz14 <rldiaz14.1@gmail.com>
2022-11-24 12:35:56 -0500

    Arimethic code

---
+++ src/Arithmetic.java
@@ -0,0 +1,14 @@
+
+public class Arithmetic {
+  public static void main(String[] args)
+  {
+    float a =  14.3f, b = 3.2f;
+    // int a =  14, b = 5;
+    // float r = a%b;
+    // float c = (float) a/b;
+    float r = a%b;
+
+    // System.out.println(c);
+    System.out.println(r);
+  }
+}
--- src/keyboard.java
+++ src/keyboard.java
@@ -7,18 +7,29 @@ public class keyboard {
 
   public static void main(String args[])
   {
-    Scanner sc = new Scanner(System.in); 
-  //   // int a, b, c;
-  //   // System.out.println("Enter 2 num");
-  // //  a = sc.nextInt();
+  Scanner sc = new Scanner(System.in); 
+  //   int a, b, c;
+  //   System.out.println("Enter 2 num");
+  //  a = sc.nextInt();
   //  b = sc.nextInt();
-  //  c = a+b;
+  //  c = (a+b)/(2*c);
   //  System.out.println("sum is " + c );
 
-  String name;
-  System.out.println("What is you name?");
-  name = sc.nextLine();
-  System.out.println("Welcome Mr/Miss" + name );
+  // String name;
+  // System.out.println("What is you name?");
+  // name = sc.nextLine();
+  // System.out.println("Welcome Mr/Miss" + name );
+
+  float base, height, area;
+  System.out.println("Enter Base and Height");
+  base = sc.nextFloat();
+  height = sc.nextFloat();
+  area=1f/2f*base*height;
+  //area=base*height/2;
+  // area = base*height*0.5f;
+  System.out.println("Area of Triangle is " + area );
+
+  
   }
   
 }
