d4874ce6b23ca9f472a817d538e2f810ac7b5e56
rldiaz14 <rldiaz14.1@gmail.com>
2022-11-23 20:16:41 -0500

    Keyboard scanner

---
+++ src/keyboard.java
@@ -0,0 +1,19 @@
+import java.lang.*;
+import java.util.*;
+
+
+
+public class keyboard {
+
+  public static void main(String args[])
+  {
+    Scanner s=new Scanner(System.in); 
+    int a, b, c;
+    System.out.println("Enter 2 num");
+   a = s.nextInt();
+   b = s.nextInt();
+   c = a+b;
+   System.out.println("sum is 20" + c );
+  }
+  
+}
