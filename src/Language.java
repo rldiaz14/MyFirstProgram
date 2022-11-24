7868f0075f0faa0e5ff3b37ced8c9cb7169dfa1f
rldiaz14 <rldiaz14.1@gmail.com>
2022-11-23 21:33:13 -0500

    language with unicode

---
+++ src/Language.java
@@ -0,0 +1,15 @@
+import java.lang.*;
+
+
+
+public class Language {
+  /**
+   * @param arg
+   */
+  public static void main(String[] arg)
+   {
+      char x = 0x0370;
+     System.out.println(x);
+   }    
+
+}
--- src/keyboard.java
+++ src/keyboard.java
@@ -7,13 +7,18 @@ public class keyboard {
 
   public static void main(String args[])
   {
-    Scanner s=new Scanner(System.in); 
-    int a, b, c;
-    System.out.println("Enter 2 num");
-   a = s.nextInt();
-   b = s.nextInt();
-   c = a+b;
-   System.out.println("sum is 20" + c );
+    Scanner sc = new Scanner(System.in); 
+  //   // int a, b, c;
+  //   // System.out.println("Enter 2 num");
+  // //  a = sc.nextInt();
+  //  b = sc.nextInt();
+  //  c = a+b;
+  //  System.out.println("sum is " + c );
+
+  String name;
+  System.out.println("What is you name?");
+  name = sc.nextLine();
+  System.out.println("Welcome Mr/Miss" + name );
   }
   
 }
